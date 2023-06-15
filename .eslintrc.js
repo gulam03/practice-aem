7a968722b558ac15521ecc1e256623d05179f718
Gulam Rabbani <Gulam.Rabbani@pfizer.com>
2023-06-15 18:49:08 +0530

    Initialize ops-testing-lint

---
+++ .eslintrc.js
@@ -0,0 +1,26 @@
+module.exports = {
+  root: true,
+  extends: ['airbnb-base', 'prettier'],
+  plugins: ['prettier'],
+  env: {
+    browser: true,
+  },
+  parser: '@babel/eslint-parser',
+  parserOptions: {
+    allowImportExportEverywhere: true,
+    sourceType: 'module',
+    requireConfigFile: false,
+  },
+  rules: {
+    // allow reassigning param
+    'no-param-reassign': [2, { props: false }],
+    'no-console': 0,
+    'linebreak-style': ['error', 'unix'],
+    'import/extensions': [
+      'error',
+      {
+        js: 'always',
+      },
+    ],
+  },
+};
