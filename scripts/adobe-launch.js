9f4116fe65ef974132cb883676062d1b38605f4d
Gulam Rabbani <Gulam.Rabbani@pfizer.com>
2023-06-15 18:49:06 +0530

    Initialize analytics-dev-prod-criteria

---
+++ scripts/adobe-launch.js
@@ -0,0 +1,23 @@
+import TagManager from './tag-manager.js';
+
+class AdobeLaunch extends TagManager {
+  setEndpoint() {
+    if (!window.placeholders.default.launchProductionUrl || !window.placeholders.default.launchNonProductionUrl) {
+      throw Error('missing placeholders for analytics endpoint');
+    }
+    if (window.location.hostname.includes('--') || window.location.hostname === 'localhost') {
+      this.endpoint = window.placeholders.default.launchNonProductionUrl;
+    } else {
+      this.endpoint = window.placeholders.default.launchProductionUrl;
+    }
+  }
+  initialize() {
+    if (!this.endpoint) return;
+    const launchScript = document.createElement('script');
+    launchScript.async = true;
+    launchScript.src = this.endpoint;
+    document.body.appendChild(launchScript);
+  }
+}
+
+export default AdobeLaunch;
